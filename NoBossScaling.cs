ddd6dd04916dffd3b97738c8767c7d3cc23ae8f0
Steviegt6 <xxlennygamerxx@gmail.com>
2022-03-27 18:16:54 -0700

    Implement config and IL edit for stat adjustments.

--- NoBossScaling.cs
+++ NoBossScaling.cs
@@ -1,8 +1,52 @@
+using Terraria;
 using Terraria.ModLoader;
 
 namespace NoBossScaling
 {
 	public class NoBossScaling : Mod
 	{
+		public override void Load()
+		{
+			base.Load();
+
+			On.Terraria.NPC.ScaleStats_ApplyMultiplayerStats += HandleScaling;
+		}
+
+		public override void Unload()
+		{
+			base.Unload();
+		}
+
+		private static void HandleScaling(
+			On.Terraria.NPC.orig_ScaleStats_ApplyMultiplayerStats orig,
+			NPC self,
+			int numPlayers,
+			float balance,
+			float boost,
+			float bossAdjustment
+		)
+		{
+			static void SetCache<T>(ref T value, T cache, bool set) => value = set ? cache : value;
+			
+			int lifeMaxCache = self.lifeMax;
+			int damageCache = self.damage;
+			int defenseCache = self.defense;
+			float scaleCache = self.scale;
+			float knockBackResistCache = self.knockBackResist;
+			
+			orig(self, numPlayers, balance, boost, bossAdjustment);
+			
+			ScalingConfig cfg = ScalingConfig.Get;
+			int type = self.type;
+			
+			SetCache(ref self.lifeMax, lifeMaxCache, cfg.ShouldScaleHealth(type));
+			SetCache(ref self.life, lifeMaxCache, cfg.ShouldScaleHealth(type));
+			SetCache(ref self.damage, damageCache, cfg.ShouldScaleDamage(type));
+			SetCache(ref self.defDamage, damageCache, cfg.ShouldScaleDamage(type));
+			SetCache(ref self.defense, defenseCache, cfg.ShouldScaleDefense(type));
+			SetCache(ref self.defDefense, defenseCache, cfg.ShouldScaleDefense(type));
+			SetCache(ref self.scale, scaleCache, cfg.ShouldScaleSize(type));
+			SetCache(ref self.knockBackResist, knockBackResistCache, cfg.ShouldScaleKb(type));
+		}
 	}
 }
---
+++ ScalingConfig.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+
+namespace NoBossScaling
+{
+    [Label("Multiplayer Scaling Config")]
+    public class ScalingConfig : ModConfig
+    {
+        public static ScalingConfig Get => ModContent.GetInstance<ScalingConfig>();
+        
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [Header("Global")]
+        [Label("Disable All HP Scaling")]
+        [Tooltip("Disables health scaling for all NPCs.")]
+        [DefaultValue(true)]
+        public bool DisableAllHealth { get; set; } = true;
+
+        [Label("Disable All Dmg. Scaling")]
+        [Tooltip("Disables damage scaling for all NPCs.")]
+        [DefaultValue(true)]
+        public bool DisableAllDamage { get; set; } = true;
+
+        [Label("Disable All Def. Scaling")]
+        [Tooltip("Disables defense scaling for all NPCs.")]
+        [DefaultValue(true)]
+        public bool DisableAllDefense { get; set; } = true;
+        
+        [Label("Disable All Size Scaling")]
+        [Tooltip("Disables size scaling for all NPCs.")]
+        [DefaultValue(true)]
+        public bool DisableAllSize { get; set; } = true;
+        
+        [Label("Disable All KB Res. Scaling")]
+        [Tooltip("Disables knockback resistance scaling for all NPCs.")]
+        [DefaultValue(true)]
+        public bool DisableAllKb { get; set; } = true;
+
+        [Header("Whitelists")]
+        [Label("Global Scaling Whitelist")]
+        [Tooltip("Whitelist NPCs that should preserve all their scaling.")]
+        public List<NPCDefinition> GlobalWhitelist { get; set; } = new();
+
+        [Label("HP Scaling Whitelist")]
+        [Tooltip("Whitelist NPCs that should preserve their health scaling.")]
+        public List<NPCDefinition> HealthWhitelist { get; set; } = new();
+        
+        [Label("Dmg. Scaling Whitelist")]
+        [Tooltip("Whitelist NPCs that should preserve their damage scaling.")]
+        public List<NPCDefinition> DamageWhitelist { get; set; } = new();
+        
+        [Label("Def. Scaling Whitelist")]
+        [Tooltip("Whitelist NPCs that should preserve their defense scaling.")]
+        public List<NPCDefinition> DefenseWhitelist { get; set; } = new();
+        
+        [Label("Size Scaling Whitelist")]
+        [Tooltip("Whitelist NPCs that should preserve their size scaling.")]
+        public List<NPCDefinition> SizeWhitelist { get; set; } = new();
+        
+        [Label("KB Res. Scaling Whitelist")]
+        [Tooltip("Whitelist NPCs that should preserve their knockback resistance scaling.")]
+        public List<NPCDefinition> KbWhitelist { get; set; } = new();
+
+        public bool IsWhitelisted(int type, List<NPCDefinition> whitelist)
+        {
+            NPCDefinition def = new(type);
+
+            return whitelist.Contains(def) || GlobalWhitelist.Contains(def);
+        }
+
+        public bool ShouldScaleHealth(int type) => !DisableAllHealth && !IsWhitelisted(type, HealthWhitelist);
+        
+        public bool ShouldScaleDamage(int type) => !DisableAllDamage && !IsWhitelisted(type, DamageWhitelist);
+        
+        public bool ShouldScaleDefense(int type) => !DisableAllDefense && !IsWhitelisted(type, DefenseWhitelist);
+        
+        public bool ShouldScaleSize(int type) => !DisableAllSize && !IsWhitelisted(type, SizeWhitelist);
+        
+        public bool ShouldScaleKb(int type) => !DisableAllKb && !IsWhitelisted(type, KbWhitelist);
+    }
+}
